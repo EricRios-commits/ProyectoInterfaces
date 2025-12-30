906de9db5552edd1d5e5d807ac1824a2fd43dc82
DiegoHdezChico <alu0101572062@ull.edu.es>
2025-12-30 12:58:41 +0000

    FEAT: Adition of Animation events for the enemies.

---
+++ InterfacesReborn/Assets/Models/Characters/Golem/Demo Scene/Scripts/HumanoidGiantDemo.cs
@@ -0,0 +1,90 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Kevin_Iglesias.Characters.Humanoid_Giant.Demo_Scene.Scripts
+{
+    [System.Serializable]
+    public class MaterialType
+    {
+        [HideInInspector]
+        public string materialName;
+        
+        public Material defaultMaterial;
+        public Material uRPMaterial;
+        
+        public SkinnedMeshRenderer[] sMR;
+        public MeshRenderer[] mR;
+    }
+
+    [ExecuteInEditMode]
+    public class HumanoidGiantDemo : MonoBehaviour
+    {
+        [SerializeField]
+        public List<MaterialType> materialTypes;
+
+        void OnValidate()
+        {
+            if(materialTypes == null)
+            {
+                return;
+            }
+            
+            for(int i = 0; i < materialTypes.Count; i++)
+            {
+                
+                if(materialTypes[i].defaultMaterial != null)
+                {
+                    materialTypes[i].materialName = materialTypes[i].defaultMaterial.name;
+                }
+                
+                for(int j = 0; j < materialTypes[i].sMR.Length; j++)
+                {
+                    if(materialTypes[i].sMR[j] != null)
+                    {
+                        if(GraphicsSettings.currentRenderPipeline == null)
+                        {
+                            if(materialTypes[i].defaultMaterial != null)
+                            {
+                                materialTypes[i].sMR[j].material = materialTypes[i].defaultMaterial;
+                            }
+                        }else{
+                            if(materialTypes[i].uRPMaterial != null)
+                            {
+                                materialTypes[i].sMR[j].material = materialTypes[i].uRPMaterial;
+                            }
+                        }
+                    }
+                }
+                
+                for(int j = 0; j < materialTypes[i].mR.Length; j++)
+                {
+                    if(materialTypes[i].mR[j] != null)
+                    {
+                        if(GraphicsSettings.currentRenderPipeline == null)
+                        {
+                            if(materialTypes[i].defaultMaterial != null)
+                            {
+                                materialTypes[i].mR[j].material = materialTypes[i].defaultMaterial;
+                            }
+                        }else{
+                            if(materialTypes[i].uRPMaterial != null)
+                            {
+                                materialTypes[i].mR[j].material = materialTypes[i].uRPMaterial;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        void OnEnable()
+        {
+            OnValidate();
+        }
+        
+        void Update(){}
+    }
+}
+#endif
